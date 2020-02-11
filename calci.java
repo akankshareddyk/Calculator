16becbbba8eea5e209cb497df250b6e0983673d1
akankshareddyk <akanksha.kasireddy@gmail.com>
2020-02-11 21:09:34 +0530

    calci.java

---
+++ calci.java
@@ -0,0 +1,49 @@
+package calculator;
+
+/**
+ *
+ * @author SUMANTH
+ */import java.util.Scanner;
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int a,b,choice;
+         float result=0;
+         /*scanner class object to read values*/
+         Scanner obj=new Scanner(System.in); 
+          
+         System.out.print("Enter first number: ");
+         a=obj.nextInt();
+         System.out.print("Enter second number: ");
+         b=obj.nextInt();
+          
+         System.out.print("\n1: Addition.\n2: Subtraction.");
+         System.out.print("\n3: Multiplication.\n4: Divide.");
+         System.out.print("\n5: Remainder.\n6: Exit.");
+          
+         System.out.print("\nEnter your choice: ");
+         choice=obj.nextInt();
+          
+         switch(choice)
+         {
+             case 1:
+                 result=(a+b); break;
+             case 2:
+                 result=(a-b); break;
+             case 3:
+                 result=(a*b); break;
+             case 4:
+                 result=(float)((float)a/(float)b); break;
+             case 5:
+                 result=(a%b); break;
+             default:
+                 System.out.println("An Invalid Choice!!!\n");
+         }
+         if(choice>=1 && choice<=5)
+            System.out.println("Result is: " + result);
+          
+     }
+}
